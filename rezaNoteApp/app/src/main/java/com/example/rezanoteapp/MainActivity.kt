aec409254cb34fe5eef390e5468ade79c08ec688
Mohammad Reza <ahmo23nn@student.ju.se>
2024-11-23 18:04:52 +0100

    mms

--- rezaNoteApp/app/src/main/java/com/example/rezanoteapp/MainActivity.kt
+++ rezaNoteApp/app/src/main/java/com/example/rezanoteapp/MainActivity.kt
@@ -6,8 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -15,9 +18,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -27,11 +32,16 @@ import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.RoundRect
 import androidx.compose.ui.graphics.Color
@@ -42,6 +52,8 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.rezanoteapp.ui.theme.RezaNoteAppTheme
+import kotlinx.coroutines.sync.Semaphore
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 data class Note(
     val id: Int,
@@ -99,7 +111,7 @@ fun NoteList(navContoller: NavController, notes: MutableList<Note>) {
                 title = { Text("Note App")  },
                 colors = TopAppBarDefaults
 
-                    .topAppBarColors(containerColor = Color.Blue, titleContentColor = MaterialTheme.colorScheme.onPrimary)
+                    .topAppBarColors(containerColor = Color.Green, titleContentColor = MaterialTheme.colorScheme.onPrimary)
             )
 
         },
@@ -154,10 +166,65 @@ fun NoteList(navContoller: NavController, notes: MutableList<Note>) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddNote(navContoller: NavController, notes: MutableList<Note>) {
+    var title by remember { mutableStateOf("") }
+    var detail by remember { mutableStateOf("") }
 
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Add Note") },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            if (title.isNotBlank() && detail.isNotBlank()) {
+                                notes.add(Note(id = notes.size, title = title, detail = detail))
+                                navContoller.popBackStack()
+                            }
+                        }) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+                    }
+                }
+            )
+        }
+    ){ padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+            //horizontalAlignment = Alignment.CenterHorizontally,
+            //verticalArrangement = Arrangement.Center
+        )
+        {
+            TextField(
+                value = title,
+                onValueChange = { title = it },
+                label = { Text("Title") }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            TextField(
+                value = detail,
+                onValueChange = { detail = it },
+                label = { Text("Details") }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = {
+                if(title.isNotBlank() && detail.isNotBlank()) {
+                    notes.add(Note(id = notes.size, title = title, detail = detail))
+                    navContoller.popBackStack()
+                }
+                })
+            {
+                Text("Save")
+            }
+        }
+
+    }
 }
+
+
 @Composable
 fun NoteDetail(navContoller: NavController, notes: Note) {
 
